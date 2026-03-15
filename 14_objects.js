0aedf6248f2b58edffc53a8436580cd1f6b9d734
tanishq13-tech <mahajantanishq70@gmail.com>
2026-03-15 12:34:15 +0530

    more about objects.

---
+++ 14_objects.js
@@ -0,0 +1,45 @@
+// const tinderUser =  new Object()  // singleton
+const tinderUser = {}   // non singleton
+tinderUser.id = "123abc"
+tinderUser.idname = "User"
+tinderUser.isLoggedIn = false
+console.log(tinderUser);
+
+const regularUser = {
+    email: "user@g.com",
+    fullname: {
+        userFullname: {
+            firstName: "user",
+            lastName: "gupta"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userFullname.firstName)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj3 = { obj1, obj2} // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+// const obj3 = Object.assign({}, obj1, obj2) // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "user0@gmail.com"
+    },
+    {
+        id: 1,
+        email: "user1@gmail.com"
+    }
+]
+
+console.log(users[1].email);
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check here the property exists or not.
+
