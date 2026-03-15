f92407750a40c426d174d8788eea56182a984eb8
tanishq13-tech <mahajantanishq70@gmail.com>
2026-03-15 22:11:05 +0530

    Basic to functions

---
+++ 15_functions.js
@@ -0,0 +1,23 @@
+function sayMyName() {
+    console.log("U");
+    console.log("S");
+    console.log("E");
+    console.log("R");
+}
+
+// sayMyName();
+
+/* function addTwoNumbers(num1, num2){
+    console.log(num1+num2);
+}
+addTwoNumbers(3,4); 
+*/
+
+function addTwoNumbers(num1, num2) {
+   /* let result = num1+num2
+    return result */
+    return num1 + num2
+}
+
+const result = addTwoNumbers(3,4)
+console.log("Result: ", result);
