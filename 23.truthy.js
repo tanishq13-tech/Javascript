0028f30d495de37667a45164c0f4a230db95b82a
tanishq13-tech <mahajantanishq70@gmail.com>
2026-03-19 19:52:37 +0530

    truthy

---
+++ 23.truthy.js
@@ -0,0 +1,41 @@
+const userEmail = "user.ai"
+if (userEmail) {
+    console.log("Got the email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+//  false , 0, -0, BigInt On, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function() {}
+
+if (userEmail.lenght === 0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??) : null  undefined
+
+let val1, val2, val3, val4;
+val1 = 5 ?? 10
+val2 = null ?? 10
+val3 = undefined ?? 15
+val4 = null ?? 10 ?? 20
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
+
+// Terniary Operator 
+// condition? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less than 80"): console.log("more than 80");
+
