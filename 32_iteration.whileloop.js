ac04dce7a38eedbb6b82f976b05c500de77ee292
tanishq13-tech <mahajantanishq70@gmail.com>
2026-03-21 21:37:04 +0530

    While and Do-while

---
+++ 32_iteration.whileloop.js
@@ -0,0 +1,20 @@
+// While
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let array = ["flash", "batman", "superman"]
+let arr = 0
+while (arr < array.length) {
+    console.log(`Value is ${array[arr]}`);
+    arr++
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
