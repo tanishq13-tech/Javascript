4eac4d9e98ce0e85eb31abb50d8c8268129de93c
tanishq13-tech <mahajantanishq70@gmail.com>
2026-03-21 22:12:07 +0530

    For In Loop

---
+++ 34_iteration.forinloop.js
@@ -0,0 +1,15 @@
+const object = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple',
+}
+for (const key in object) {
+    console.log(`${key} shortcut is for ${object[key]}`);
+}
+
+const programing = ["js", "rb", "py"]
+for (const key in programing) {
+    console.log(programing[key]);
+    
+}
